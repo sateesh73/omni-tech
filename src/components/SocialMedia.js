e2978c56d4507456834cd09c09099cc7e671ad59
Sateesh Varma <sateeshvarma73@gmail.com>
2022-10-25 21:57:47 +0530

    update url

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FaFacebook} from "react-icons/fa"
 import {AiFillInstagram, AiFillTwitterCircle, AiOutlineWhatsApp} from "react-icons/ai"
-import { Link } from 'react-router-dom';
 const SocialMedia = (props) => {
     const item = props.classItem;
     const faceUrl = props.faceUrl;
@@ -11,10 +10,10 @@ const SocialMedia = (props) => {
 
   return (
     <div className={item}>
-      <Link to={instaUrl} target="_blank" rel="noopener noreferrer"><AiFillInstagram size={30} className='text-white hover:text-red hover:scale-110 duration-200'/> </Link>   
-      <Link to={faceUrl} target="_blank" rel="noopener noreferrer"><FaFacebook size={30} className='text-white hover:text-red hover:scale-110 duration-200'/> </Link>
-      <Link to={tweetUrl} target="_blank" rel="noopener noreferrer"><AiFillTwitterCircle size={30} className='text-white hover:text-red hover:scale-110 duration-200'/></Link>
-      <Link to={whatUrl} target="_blank" rel="noopener noreferrer"><AiOutlineWhatsApp size={30} className='text-white hover:text-red hover:scale-110 duration-200'/></Link>
+      <a href={instaUrl} target="_blank" rel="noopener noreferrer"><AiFillInstagram size={30} className='text-white hover:text-red hover:scale-110 duration-200'/> </a>   
+      <a href={faceUrl} target="_blank" rel="noopener noreferrer"><FaFacebook size={30} className='text-white hover:text-red hover:scale-110 duration-200'/> </a>
+      <a href={tweetUrl} target="_blank" rel="noopener noreferrer"><AiFillTwitterCircle size={30} className='text-white hover:text-red hover:scale-110 duration-200'/></a>
+      <a href={whatUrl} target="_blank" rel="noopener noreferrer"><AiOutlineWhatsApp size={30} className='text-white hover:text-red hover:scale-110 duration-200'/></a>
     </div>
   )
 }
